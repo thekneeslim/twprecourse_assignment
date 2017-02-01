2403645a37403cce258e326a1e9bd3bcbb2ec3a3
hiko_seijuro1000 <p16f1956>
2017-02-02 00:32:50 +0800

    inserting main file

---
+++ Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Assignment 1
+        System.out.println("----- ASSIGNMENT 1 -----");
+        System.out.println("*");
+
+    // Assignment 2
+        System.out.println("----- ASSIGNMENT 2 -----");
+        Triangle assignment2 = new Triangle();
+        assignment2.horizontalLine(3);
+        System.out.println();
+
+    // Assignment 3
+        System.out.println("----- ASSIGNMENT 3 -----");
+        Triangle assignment3 = new Triangle();
+        assignment3.verticleLine(3);
+
+    // Assignment 4
+        System.out.println("----- ASSIGNMENT 4 -----");
+        Triangle assignment4 = new Triangle();
+        assignment4.rightTriangle(4);
+
+     // Assignment 5
+         System.out.println("----- ASSIGNMENT 5 -----");
+    }
+
+
+}
