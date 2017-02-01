b08970ced8e1649bdd559297bfa5e55a39f65e83
hiko_seijuro1000 <p16f1956>
2017-02-02 00:30:53 +0800

    First commit ever

---
+++ Triangle.java
@@ -0,0 +1,27 @@
+package com.company;
+
+/**
+ * Created by hiko_seijuro1000 on 1/2/17.
+ */
+public class Triangle {
+    public void horizontalLine(int arg) {
+        for (int i = 0; i < arg; i++) {
+            System.out.print("*");
+        }
+    }
+
+    public void verticleLine(int arg) {
+        for (int i = 0; i < arg; i ++) {
+            System.out.println("*");
+        }
+    }
+
+    public void rightTriangle(int arg) {
+        int number = 1;
+        String answer = new String("");
+        for (int i = 0; i < arg; i++) {
+            answer += "*";
+            System.out.println(answer);
+        }
+    }
+}
